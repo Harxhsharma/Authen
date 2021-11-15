db3f1daf3be3a05eaeeba19279678b4b49396edd
Harsh Sharma <76914718+Harxhsharma@users.noreply.github.com>
2021-11-16 00:36:28 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
-STATIC_URL="/static/"
+
+STATIC_URL='/static/'
 django_heroku.settings(locals())
 
 # Default primary key field type
